62b3d6213d6bdcd5aff09a6c3166509de1b7979f
Mustafa-Dev <mustafaq@zenkoders.com>
2024-05-20 20:18:13 +0500

    basic features added

--- App.tsx
+++ App.tsx
@@ -5,26 +5,59 @@
  * @format
  */
 
-import React from 'react';
-import {Text, View} from 'react-native';
-import {
-  ScreenCapturePickerView,
-  RTCPeerConnection,
-  RTCIceCandidate,
-  RTCSessionDescription,
-  RTCView,
-  MediaStream,
-  MediaStreamTrack,
-  mediaDevices,
-  registerGlobals,
-} from 'react-native-webrtc';
+import React, {useState} from 'react';
+import {Button, SafeAreaView, StyleSheet, View, StatusBar} from 'react-native';
+import {mediaDevices, RTCView} from 'react-native-webrtc';
 
 function App(): React.JSX.Element {
+  const [stream, setStream] = useState(null);
+  const start = async () => {
+    console.log('start');
+    if (!stream) {
+      try {
+        const s = await mediaDevices.getUserMedia({video: true});
+        setStream(s);
+      } catch (e) {
+        console.error(e);
+      }
+    }
+  };
+  const stop = () => {
+    console.log('stop');
+    if (stream) {
+      stream.release();
+      setStream(null);
+    }
+  };
   return (
-    <View>
-      <Text>abdghsdevdegs</Text>
-    </View>
+    <>
+      <StatusBar barStyle="dark-content" />
+      <SafeAreaView style={styles.body}>
+        {stream && <RTCView streamURL={stream.toURL()} style={styles.stream} />}
+        <View style={styles.footer}>
+          <Button title="Start" onPress={start} />
+          <Button title="Stop" onPress={stop} />
+        </View>
+      </SafeAreaView>
+    </>
   );
 }
 
 export default App;
+
+const styles = StyleSheet.create({
+  body: {
+    backgroundColor: '#fff',
+    ...StyleSheet.absoluteFill,
+  },
+  stream: {
+    flex: 1,
+  },
+  footer: {
+    backgroundColor: '#fff',
+    position: 'absolute',
+    bottom: 10,
+    left: 0,
+    right: 0,
+  },
+});
